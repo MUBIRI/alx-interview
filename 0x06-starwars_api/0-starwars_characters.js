75d54819f6c3217ea26c24ddeb7f8ab9dd7d458f
MUBIRI <mubirirobberts@gmail.com>
2023-05-10 11:22:07 +0000

    Debug fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,25 @@
 #!/usr/bin/node
-//Star Wars API
 const request = require('request');
-const urlMovie = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-//make a request to the Star Wars API
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-
-request(urlMovie, async function (error, response, body) {
-  const arr = [];
-//calling the arg urlmovie
-  if (error) {
-    console.log(error);
-  } else {
-    const film = JSON.parse(body);
-    for (let i = 0; i < film.characters.length; i++) {
-      arr.push(myCharacter(film.characters[i]));
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
     }
-  }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-  let actors = await Promise.all(arr);
-
-  actors = actors.map((actor) => JSON.parse(actor).name);
-  actors.forEach((actor) => console.log(actor));
-});
-///mapping over the results
-function myCharacter (thisCharacter) {
-  return new Promise((resolve, reject) => {
-    request(thisCharacter, function (error, response, body) {
-      if (error) {
-        reject(error);
-      }
-      resolve(body);
-    });
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
-
